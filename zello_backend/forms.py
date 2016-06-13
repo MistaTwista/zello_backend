dd4dcd84fb3d7d36c8be6bfcd0b16f248d9ab56f
Maxim Kernozhitskiy <maxim@aeromultimedia.com>
2016-06-13 03:22:43 +0300

    Posts can be added to blog

---
+++ zello_backend/forms.py
@@ -0,0 +1,20 @@
+from wtforms import (
+    Form,
+    StringField,
+    TextAreaField,
+    HiddenField,
+    validators,
+)
+
+strip_filter = lambda x: x.strip() if x else None
+
+
+class BlogCreateForm(Form):
+    title = StringField('Title', [validators.Length(min=1, max=255)],
+                        filters=[strip_filter])
+    body = TextAreaField('Contents', [validators.Length(min=1)],
+                        filters=[strip_filter])
+
+
+class BlogUpdateForm(BlogCreateForm):
+    id = HiddenField()
--- zello_backend/views/blog.py
+++ zello_backend/views/blog.py
@@ -2,6 +2,7 @@ from pyramid.view import view_config
 from pyramid.httpexceptions import HTTPFound, HTTPNotFound
 from ..models.blog_record import BlogRecord
 from ..services.blog_record import BlogRecordService
+from ..forms import BlogCreateForm, BlogUpdateForm
 
 
 @view_config(route_name='blog',
@@ -17,10 +18,26 @@ def blog_view(request):
 @view_config(route_name='blog_action', match_param='action=create',
              renderer='zello_backend:templates/edit_blog.jinja2')
 def blog_create(request):
-    return {}
+    entry = BlogRecord()
+    form = BlogCreateForm(request.POST)
+    if request.method == 'POST' and form.validate():
+        form.populate_obj(entry)
+        request.dbsession.add(entry)
+        return HTTPFound(location=request.route_url('home'))
+    return {'form': form, 'action': request.matchdict.get('action')}
 
 
 @view_config(route_name='blog_action', match_param='action=edit',
              renderer='zello_backend:templates/edit_blog.jinja2')
 def blog_update(request):
-    return {}
+    blog_id = int(request.params.get('id', -1))
+    entry = BlogRecordService.by_id(blog_id, request)
+    if not entry:
+        return HTTPNotFound()
+    form = BlogUpdateForm(request.POST, entry)
+    if request.method == 'POST' and form.validate():
+        form.populate_obj(entry)
+        return HTTPFound(
+            location=request.route_url('blog', id=entry.id, slug=entry.slug))
+    return {'form': form, 'action': request.matchdict.get('action')}
+
